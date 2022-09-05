3bc61a7948b75919493f32468d755b393abe6684
Paul Ruvolo <paullundyruvolo@gmail.com>
2022-09-05 11:24:22 -0400

    added skeleton of sample solution for day 2

---
+++ in_class_day02_solutions/in_class_day02_solutions/test_message.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from in_class_day02_solutions.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ in_class_day02_solutions/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'in_class_day02_solutions'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='parallels',
+    maintainer_email='paullundyruvolo@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'test_message = in_class_day02_solutions.test_message:main'
+        ],
+    },
+)
