d58d65e18b2b800c61dc6953dc56e2bab894759d
Paul Ruvolo <paullundyruvolo@gmail.com>
2022-09-05 11:32:50 -0400

    minor change

--- in_class_day02_solutions/in_class_day02_solutions/test_message.py
+++ in_class_day02_solutions/in_class_day02_solutions/test_message.py
@@ -1,5 +1,8 @@
+""" This script explores publishing ROS messages in ROS using Python """
+from geometry_msgs.msg import PointStamped
+
 def main():
-    print('Hi from in_class_day02_solutions.')
+    print('Hi from in_class_day02_solutions2 .')
 
 
 if __name__ == '__main__':
