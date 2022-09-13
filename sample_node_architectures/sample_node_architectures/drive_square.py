37e5bc84fe49c9c4a1f96a775fa0008528f4c575
Paul Ruvolo <paullundyruvolo@gmail.com>
2022-09-13 16:57:58 -0400

    added a drive square node

---
+++ sample_node_architectures/sample_node_architectures/drive_square.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from sample_node_architectures.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ sample_node_architectures/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'sample_node_architectures'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='pruvolo',
+    maintainer_email='paullundyruvolo@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'drive_square = sample_node_architectures.drive_square:main'
+        ],
+    },
+)
