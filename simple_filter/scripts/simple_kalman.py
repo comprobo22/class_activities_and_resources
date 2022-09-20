c987252d90c91611b9d66371fed1d19637a39d8a
Paul Ruvolo <paullundyruvolo@gmail.com>
2022-09-19 23:52:51 -0400

    ported simple kalman filter

--- simple_filter/scripts/simple_kalman.py
+++ simple_filter/scripts/simple_kalman.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 
-# TODO: not ported yet
-
 """
     This script implements a Kalman filter for the system:
 
@@ -11,12 +9,13 @@
 """
 
 import matplotlib.pyplot as plt
-import rospy
+import rclpy
+from rclpy.node import Node
 from numpy import arange
 from numpy.random import randn
 from math import e, sqrt, pi
-from dynamic_reconfigure.server import Server
-from simple_filter.cfg import SimpleKalmanConfig
+from rclpy.parameter import Parameter
+from rcl_interfaces.msg import SetParametersResult
 
 class SimpleWorld(object):
     """ A simple system with dynamics:
@@ -44,39 +43,51 @@ class SimpleWorld(object):
         self.x_true = self.x_true + sqrt(self.sigma_m_sq)*randn()
         return self.x_true
 
-class SimpleKalmanFilter(object):
+class SimpleKalmanFilter(Node):
     """ A Kalman filter node that estimates a single state x_t using noisy position measurements """
 
     def __init__(self):
         """ Sets up the world model and loads initial parameters """
-        rospy.init_node('simple_kalman')
+        super().__init__('simple_kalman')
         plt.ion()
 
         # initial beliefs
         self.mu = 0
         self.sigma_sq = 1
-
-        # motor noise
-        sigma_m_sq = rospy.get_param('~sigma_m_sq', 0.01)
-        # observation noise
-        sigma_z_sq = rospy.get_param('~sigma_z_sq', .1)
+        (sigma_m_sq, sigma_z_sq, pause_time) = self.declare_parameters(
+            namespace='',
+            parameters=[
+                ('sigma_m_sq',  0.01),
+                ('sigma_z_sq', 0.1),
+                ('pause_time', 0.5)
+            ]
+        )
         # time to pause between plots
-        self.pause_time = rospy.get_param('~pause_time', 0.5)
+        self.pause_time = pause_time.value
 
         self.graphs = None
-        self.world = SimpleWorld(self.mu, self.sigma_sq, sigma_m_sq, sigma_z_sq)
+        self.world = SimpleWorld(self.mu, self.sigma_sq, sigma_m_sq.value, sigma_z_sq.value)
+        self.create_timer(0.01, self.run_loop)
+        self.last_update = self.get_clock().now()
+        self.add_on_set_parameters_callback(self.parameter_callback)
 
-        srv = Server(SimpleKalmanConfig, self.config_callback)
+    def parameter_callback(self, params):
+        """ This callback allows the parameters of the node to be adjusted
+            dynamically by other ROS nodes (e.g., dynamic_reconfigure).
+            This function is only needed to support dynamic_reconfigure. """
+        for param in params:
+            if param.name == 'pause_time' and param.type_ == Parameter.Type.DOUBLE:
+                self.pause_time = param.value
+            elif param.name == 'sigma_m_sq' and param.type_ == Parameter.Type.DOUBLE:
+                self.world.sigma_m_sq = param.value
+            elif param.name == 'sigma_z_sq' and param.type_ == Parameter.Type.DOUBLE:
+                self.world.sigma_z_sq = param.value
+        print(self.pause_time, self.world.sigma_m_sq, self.world.sigma_z_sq)
+        return SetParametersResult(successful=True)
 
-    def config_callback(self, config, level):
-        """ Get the pause_time, movement noise, and measurement noise """
-        self.pause_time = config['pause_time']
-        self.world.sigma_m_sq = config['sigma_m_sq']
-        self.world.sigma_z_sq = config['sigma_z_sq']
-        return config
-
-    def run(self):
-        while not rospy.is_shutdown():
+    def run_loop(self):
+        if self.get_clock().now() - self.last_update > rclpy.time.Duration(seconds=self.pause_time):
+            self.last_update = self.get_clock().now()
             # Graph new observation from the system
             z_t = self.world.get_z_t()
             self.graphs = self.plot_pdf(z_t)
@@ -85,12 +96,11 @@ class SimpleKalmanFilter(object):
             K_t = (self.sigma_sq + self.world.sigma_m_sq)/(self.sigma_sq + self.world.sigma_m_sq + self.world.sigma_z_sq)
             self.mu = self.mu + K_t*(z_t - self.mu)
             self.sigma_sq = (1-K_t)*(self.sigma_sq+self.world.sigma_m_sq)
-            plt.pause(self.pause_time)
             self.graphs = self.plot_pdf(z_t)
 
             # sample next state
             self.world.get_x_t()
-            plt.pause(self.pause_time)
+            plt.pause(0.01)
 
     def plot_pdf(self, z):
         """ Plot the Gaussian PDF with the specified mean (mu) and variance (sigma_sq)
@@ -116,6 +126,11 @@ class SimpleKalmanFilter(object):
         plt.show(block=False)
         return self.graphs
 
+def main(args=None):
+    rclpy.init(args=args)      # Initialize communication with ROS
+    node = SimpleKalmanFilter()   # Create our Node
+    rclpy.spin(node)           # Run the Node until ready to shutdown
+    rclpy.shutdown()           # cleanup
+
 if __name__ == '__main__':
-    node = SimpleKalmanFilter()
-    node.run()
+    main()
