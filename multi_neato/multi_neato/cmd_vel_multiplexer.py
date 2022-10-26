df4674ef2bbeaff1ecd40b6a32fbc6850478588a
Paul Ruvolo <paullundyruvolo@gmail.com>
2022-10-26 13:28:57 -0400

    added a package for multi neato examples

---
+++ multi_neato/multi_neato/cmd_vel_multiplexer.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from multi_neato.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ multi_neato/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'multi_neato'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='pruvolo',
+    maintainer_email='paullundyruvolo@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'cmd_vel_multiplexer = multi_neato.cmd_vel_multiplexer:main'
+        ],
+    },
+)
